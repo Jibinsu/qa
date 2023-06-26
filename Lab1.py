44ce4f77ebcb0c0acd5e725928b4994c041e88fc
Jibinsu <jake@tabology.com>
2023-06-26 13:55:44 +0100

    lab1 into repo

---
+++ Lab1.py
@@ -0,0 +1,32 @@
+# all code will be commented out for testing
+
+
+
+# part 1
+#print('Hello World')
+
+#part 2
+#username= 'Bob'
+#age='32'
+#print(username,'is',age,'years old')
+
+#part 2.5
+#username= 'Bob'
+#age='32'
+#print(username+'is'+age+'years old')
+
+#part 2.8
+#username= 'Bob'
+#age='32'
+#print(username, 'is' + age + 'years old')
+ 
+#part 2 (get user input)
+#username=input('Please enter your name')
+#age=input('Please enter your age') 
+#print(username,'is',age,'years old')
+
+length=4
+print(length)
+
+width=2
+print(width)
