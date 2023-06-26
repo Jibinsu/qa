9acfa73e5a151e68d88a92b4c38aa61105f7c5b6
Jibinsu <jake@tabology.com>
2023-06-26 14:58:38 +0100

    remove commenting

--- Lab1.py
+++ Lab1.py
@@ -3,27 +3,27 @@
 
 
 # part 1
-#print('Hello World')
+print('Hello World')
 
 #part 2
-#username= 'Bob'
-#age='32'
-#print(username,'is',age,'years old')
+username= 'Bob'
+age='32'
+print(username,'is',age,'years old')
 
 #part 2.5
-#username= 'Bob'
-#age='32'
-#print(username+'is'+age+'years old')
+username= 'Bob'
+age='32'
+print(username+'is'+age+'years old')
 
 #part 2.8
-#username= 'Bob'
-#age='32'
-#print(username, 'is' + age + 'years old')
+username= 'Bob'
+age='32'
+print(username, 'is' + age + 'years old')
  
 #part 2 (get user input)
-#username=input('Please enter your name')
-#age=input('Please enter your age') 
-#print(username,'is',age,'years old')
+username=input('Please enter your name')
+age=input('Please enter your age') 
+print(username,'is',age,'years old')
 
 #rectangle time :) 
 length = int(input("4:"))
