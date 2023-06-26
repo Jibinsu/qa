c57f7f4cb25ba0ce277f432d8349e8380c08deae
Jibinsu <jake@tabology.com>
2023-06-26 15:42:45 +0100

    changed input on rectangle int

--- Lab1.py
+++ Lab1.py
@@ -26,9 +26,9 @@ age=input('Please enter your age')
 print(username,'is',age,'years old')
 
 #rectangle time :) 
-length = int(input("4:"))
+length = int(input("4"))
 
-width = int(input("2:"))
+width = int(input("2"))
 
 perimeter = 2 * (length + width)
 
