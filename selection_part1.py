b61586ad9960d8c153724da129d98d957d3aabd8
Jibinsu <jake@tabology.com>
2023-06-27 13:37:57 +0100

    ---

--- selection_part1.py
+++ selection_part1.py
@@ -17,44 +17,39 @@
 
 # Calculator task
 
-def add(x, y):
-    return x + y
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
-def subtract(x, y):
-    return x - y
+print("Calculator Menu:")
+print("1. Add +")
+print("2. Subtract -")
+print("3. Multiply *")
+print("4. Divide /")
+print("5. Square s")
 
-def multiply(x, y):
-    return x * y
+choice = input("Enter your choice (1-5): ")
 
-def divide(x, y):
-    if y != 0:
-        return x / y
+if choice == '1':
+    result = num1 + num2
+    print("The sum is:", result)
+elif choice == '2':
+    result = num1 - num2
+    print("The difference is:", result)
+elif choice == '3':
+    result = num1 * num2
+    print("The product is:", result)
+elif choice == '4':
+    if num2 != 0:
+        result = num1 / num2
+        print("The quotient is:", result)
     else:
-        return "Cannot divide by zero"
+        print("Error: Division by zero is not allowed.")
+elif choice == '5':
+    result1 = num1 ** 2
+    result2 = num2 ** 2
+    print("Square of", num1, "is:", result1)
+    print("Square of", num2, "is:", result2)
+else:
+    print("Invalid choice. Please select a valid option (1-5).")
 
-print("Calculator App!")
-
-while True:
-    print("\nSelect an operation:")
-    print("1. Add +")
-    print("2. Subtract -")
-    print("3. Multiply * ")
-    print("4. Divide / ")
-
-    choice = input("Enter your choice (1-4):")
-
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-
-    if choice == '1':
-        result = add(num1, num2)
-        print(f"The result of {num1} + {num2} is: {result}")
-    elif choice == '2':
-        result = subtract(num1, num2)
-        print(f"The result of {num1} - {num2} is: {result}")
-    elif choice == '3':
-        result = multiply(num1, num2)
-        print(f"The result of {num1} * {num2} is: {result}")
-    elif choice == '4':
-        result = divide(num1, num2)
-        print(f"The result of {num1} / {num2} is: {result}")
+print("** End **")
